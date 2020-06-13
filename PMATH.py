34d2a9b1d7473e4495478c5c7fbb329ceb163a2a
Paul Thorel <56446246+Paul-Thorel@users.noreply.github.com>
2020-06-13 10:20:20 +0200

    Update PMATH.py

--- PMATH.py
+++ PMATH.py
@@ -1,7 +1,3 @@
-import math
-
-print(dir(math))
-
 from math import sqrt as _s
 
 def factorial(x):
@@ -13,20 +9,5 @@ def isclose(a,b,rel_tol=1e-09,abs_tol=0.0):
  return abs(a-b)<=max(rel_tol*max(abs(a),abs(b)),abs_tol)
 
 def hypot(x,y):return _s(x*x+y*y)
-
-def add(a,b):return a+b
-
-def substract(a,b):return a+b
-
-def multiply(a,b):return a*b
-
-def fma(x,y,z):return x*y+z
-
-def divide(x,y):return x/y
-
-def power(x,y,modulo=None):
- s=x**y
- if modulo:s%=modulo
- return s
 
 def copy_negate(x):return -x
